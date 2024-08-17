ec04b2d395582d3afb864ce695a550b785d809fa
Justin Shilling <loveofmusic25@hotmail.com>
2024-08-17 14:48:55 -0400

    add daisyUI color theme config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,17 @@ export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}', './public/index.html'],
   theme: {},
   plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#0b6285',
+          secondary: '#6b08db',
+          accent: '#09eff7',
+          neutral: '#3d4451',
+          'base-100': '#ffffff',
+        },
+      },
+    ],
+  },
 };
