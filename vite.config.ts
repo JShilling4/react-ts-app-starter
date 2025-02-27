a2e3be7bd688abc2b9729c2d2f3a34235bbe083d
Justin Shilling <loveofmusic25@hotmail.com>
2025-02-27 09:28:57 -0500

    update packages, adjust config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { UserConfig } from 'vitest/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()] as UserConfig['plugins'],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: 'jsdom',
